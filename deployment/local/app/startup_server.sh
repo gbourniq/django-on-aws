97b4af13b11566aa032fbcf07f5c4d9e92690f2d
Guillaume Bournique <gbournique@gmail.com>
2021-01-25 19:16:57 +0100

    add awscli to for container to collectstatic files to S3

--- deployment/local/app/startup_server.sh
+++ deployment/local/app/startup_server.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
 
+
+if [[ ! -z $STATICFILES_BUCKET ]]; then
+    echo "STATICFILES_BUCKET is set, assuming AWS deployment"
+    echo "Enable public access for Django collectstatic"
+    aws s3api put-public-access-block \
+        --bucket dev.bournique.fr-static-files \
+        --public-access-block-configuration "BlockPublicAcls=true,IgnorePublicAcls=true,BlockPublicPolicy=true,RestrictPublicBuckets=true" || true
+fi
+
 echo "Collecting static files"
 python manage.py collectstatic --no-input -v 0
+
+if [[ ! -z $STATICFILES_BUCKET ]]; then
+    echo "Disable public access for Django collectstatic"
+    aws s3api put-public-access-block \
+        --bucket dev.bournique.fr-static-files \
+        --public-access-block-configuration "BlockPublicAcls=false,IgnorePublicAcls=false,BlockPublicPolicy=false,RestrictPublicBuckets=false" || true
+fi
 
 echo "Running migrations"
 python manage.py makemigrations main
