f3105cc1cc47ec303beaa3207aee42aff4878f4d
Guillaume Bournique <gbournique@gmail.com>
2021-01-27 00:44:21 +0100

    fixed s3 static files, forward container logs and django logs to CW logs/dashboard, add ec2 instances details to footer.html

--- deployment/aws/codedeploy-app/scripts/3_after_install.sh
+++ deployment/aws/codedeploy-app/scripts/3_after_install.sh
@@ -2,8 +2,3 @@
 
 set -x
 
-EC2_INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-EC2_AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
-
-# Assume /var/www/html/index.html was created at the Install Hook
-# sed -i "s/was deployed/was deployed to deployment group $DEPLOYMENT_GROUP_NAME on $(hostname -f) ($EC2_INSTANCE_ID) in $EC2_AZ/g" /var/www/html/index.html
--- deployment/aws/codedeploy-app/scripts/4_start_server.sh
+++ deployment/aws/codedeploy-app/scripts/4_start_server.sh
@@ -5,6 +5,7 @@ set -x
 # Variables - TODO: remove hardcoded vars
 IMAGE_REPOSITORY=gbournique/django-on-aws
 TAG=1.0.0
+CONTAINER_NAME=myapp
 
 # Source environment variables set from cfn-init
 source /root/.bashrc
@@ -16,6 +17,7 @@ systemctl stop httpd.service
 docker pull ${IMAGE_REPOSITORY}:${TAG}
 docker run -d \
     -p 80:8080 \
+    --name=${CONTAINER_NAME} \
     --restart=no \
     --log-driver=awslogs \
     --log-opt awslogs-group=${DOCKER_LOGS_LOG_GROUP_NAME} \
@@ -27,3 +29,9 @@ docker run -d \
     --env AWS_S3_CUSTOM_DOMAIN=${WEBAPP_DOMAIN} \
     --env DEBUG=True \
     ${IMAGE_REPOSITORY}:${TAG}
+
+# Write some instance details to the footer.html file
+EC2_INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+EC2_AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
+HTML_PAGE=/home/portfoliouser/app/main/templates/main/includes/footer.html
+docker exec $CONTAINER_NAME sed -i "s/aws-ec2-details-placeholder/Running on AWS | $(hostname -f) | $EC2_INSTANCE_ID | $EC2_AZ/g" $HTML_PAGE
